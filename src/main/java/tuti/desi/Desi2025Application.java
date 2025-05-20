decf85cb13384f53ac5b6c89a95feb19e96fb789
Juan Sasia <juanignaciosasia@gmail.com>
2025-05-19 21:43:33 -0300

    Inicio del proyecto

---
+++ src/main/java/tuti/desi/Desi2025Application.java
@@ -0,0 +1,13 @@
+package tuti.desi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Desi2025Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Desi2025Application.class, args);
+	}
+
+}
